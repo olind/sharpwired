1bc58de1b33d955b54472c4192828ec5816937a0
olalindberg <olalindberg@cd302ba0-9ce7-11dd-979b-47109e308e52>
2007-12-13 18:31:47 +0000

    And as usual I forgot to attach the new file!

---
+++ SharpWired/SourceCode/Model/ServerInformation.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpWired.MessageEvents;
+
+namespace SharpWired.Model
+{
+    /// <summary>
+    /// Represents the connected server
+    /// </summary>
+    public class ServerInformation
+    {
+        private MessageEventArgs_200 messageEventArgs;
+        private string appVersion;
+        private int filesCount;
+        private int fileSize;
+        private string protocolVersion;
+        private string serverDescription;
+        private string serverName;
+        private DateTime startTime;
+        private bool connected;
+
+        /// <summary>
+        /// Get or set the server app version
+        /// </summary>
+        public string AppVersion
+        {
+            get { return appVersion; }
+            set { appVersion = value; }
+        }
+
+        /// <summary>
+        /// Get or set the servers file count
+        /// </summary>
+        public int FilesCount
+        {
+            get { return filesCount; }
+            set { filesCount = value; }
+        }
+
+        /// <summary>
+        /// Get or set the file size on the server
+        /// </summary>
+        public int FileSize
+        {
+            get { return fileSize; }
+            set { fileSize = value; }
+        }
+
+        /// <summary>
+        /// Get or set the server protocol version
+        /// </summary>
+        public string ProtocolVersion
+        {
+            get { return protocolVersion; }
+            set { protocolVersion = value; }
+        }
+
+        /// <summary>
+        /// Get or set the server description
+        /// </summary>
+        public string ServerDescription
+        {
+            get { return serverDescription; }
+            set { serverDescription = value; }
+        }
+
+        /// <summary>
+        /// Get or set the server name
+        /// </summary>
+        public string ServerName
+        {
+            get { return serverName; }
+            set { serverName = value; }
+        }
+
+        /// <summary>
+        /// Get or set the server start time
+        /// </summary>
+        public DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+
+        /// <summary>
+        /// Get or set the online status
+        /// </summary>
+        public bool Connected
+        {
+            get {
+                if (ServerOnlineStatusChangedEvent != null)
+                {
+                    ServerOnlineStatusChangedEvent(connected);
+                }
+                return connected;
+            }
+            set { 
+                connected = value;
+                if (ServerOnlineStatusChangedEvent != null)
+                {
+                    ServerOnlineStatusChangedEvent(connected);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Delegate for ServerOnlineStatusChangedEvent
+        /// </summary>
+        /// <param name="connected"></param>
+        public delegate void ServerOnlineStatusChangedDelegate(bool connected);
+        /// <summary>
+        /// Event raised when the server online status changed ie you get connected or disconnected
+        /// </summary>
+        public event ServerOnlineStatusChangedDelegate ServerOnlineStatusChangedEvent;
+
+        /// <summary>
+        /// Init the server information when we are online
+        /// </summary>
+        /// <param name="messageEventArgs"></param>
+        public void Init(MessageEventArgs_200 messageEventArgs)
+        {
+            this.messageEventArgs = messageEventArgs;
+            this.appVersion = messageEventArgs.AppVersion;
+            this.filesCount = messageEventArgs.FilesCount;
+            this.fileSize = messageEventArgs.FilesSize;
+            this.protocolVersion = messageEventArgs.ProtocolVersion;
+            this.serverDescription = messageEventArgs.ServerDescription;
+            this.serverName = messageEventArgs.ServerName;
+            this.startTime = messageEventArgs.StartTime;
+        }
+
+        /// <summary>
+        /// Constructor - Empty
+        /// </summary>
+        /// <param name="messageEventArgs"></param>
+        public ServerInformation()
+        {
+        }
+    }
+}
