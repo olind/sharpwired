500b4a4f2a12d6fd612cdf25f04e762d462d8624
eproxus <eproxus@cd302ba0-9ce7-11dd-979b-47109e308e52>
2008-03-09 20:50:23 +0000

    Forgot to add WiredListViewItem.cs

---
+++ SharpWired/SourceCode/Gui/Chat/WiredListViewItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using SharpWired.Model.Users;
+
+namespace SharpWired.Gui.Chat {
+    class WiredListViewItem : ListViewItem {
+        private UserItem userItem;
+
+        public UserItem UserItem {
+            get { return userItem; }
+            set { userItem = value; }
+        }
+
+        public WiredListViewItem(UserItem user, string[] subItems, string imageKey)
+            : base(subItems, imageKey) { 
+            this.UserItem = user;
+        }
+    }
+}
