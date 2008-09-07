8988a5a55c5a1181ec155779f1d19b1d03ac1a86
eproxus <eproxus@cd302ba0-9ce7-11dd-979b-47109e308e52>
2008-09-07 08:56:10 +0000

    Added the forgotten TransferContainer.cs & Co!

---
+++ SharpWired/SourceCode/Gui/Transfers/TransferContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using SharpWired.Model;
+using SharpWired.Controller;
+
+namespace SharpWired.Gui.Transfers {
+    public partial class TransferContainer : SharpWiredGuiBase {
+        public TransferContainer() {
+            InitializeComponent();
+        }
+
+        public void Init(SharpWiredModel model, SharpWiredController controller) {
+            this.transferList1.Init(model, controller);
+        }
+    }
+}
