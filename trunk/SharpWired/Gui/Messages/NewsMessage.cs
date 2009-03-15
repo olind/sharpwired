557dd35d181153c90772d88bf60ab323c2930d00
lindberg.ola <lindberg.ola@cd302ba0-9ce7-11dd-979b-47109e308e52>
2009-03-15 07:42:52 +0000

    Added the colors for the nick names to the News window as well.

--- trunk/SharpWired/Gui/Messages/NewsMessage.cs
+++ trunk/SharpWired/Gui/Messages/NewsMessage.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using SharpWired.Model.News;
+using SharpWired.Model.Users;
+using System.Drawing;
 
 namespace SharpWired.Gui.Messages {
     class NewsMessage : SharpWiredGuiBase, IPrintableHTML {
@@ -14,15 +16,18 @@ namespace SharpWired.Gui.Messages {
         }
 
         public string ToHTML() {
+
             var divClass = "";
             if (post.Nick == Model.Server.User.Nick) {
                 divClass = " class=\"me\"";
             }
 
+            NickColor nc = new NickColor(post.Nick);
+
             return
 "<div" + divClass + @">
 	<span class=""time"">" + post.Time.ToShortTimeString() + @"</span>
-	<span class=""user"">" + post.Nick + @"</span>
+	<span class=""user"" style=""color: " + ColorTranslator.ToHtml(nc.RGB) + "\">" + post.Nick + @"</span>
 	<span class=""text"">
 		<p>
 			" + post.Post.Replace("\n", "<br/>\n") + @"
