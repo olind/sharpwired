e55a4a50fcb9b0e7c0ef0f09c85545229267777b
lindberg.ola <lindberg.ola@cd302ba0-9ce7-11dd-979b-47109e308e52>
2009-03-21 10:32:15 +0000

    Added (failing) test for issue #12.

--- Tests/Model/Files/FolderTest.cs
+++ Tests/Model/Files/FolderTest.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿using NUnit.Framework.SyntaxHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using SharpWired.MessageEvents;
 using SharpWired.Model.Files;
 
 namespace SharpWired.Tests.Model.Files {
     [TestFixture]
-    public class FolderTest {}
+    public class FolderTest {
+    	Folder parentFolder = new Folder("/parent", DateTime.MinValue, DateTime.MinValue, 1);
+    	
+    	private List<MessageEventArgs_410420> getListWithOneChildren(){
+    		int messageid = -1;
+    		string messageName = "test";
+    		string path = "/parent/child";
+    		FileType fileType = FileType.FOLDER;
+    		int fileSize = 1;
+    		DateTime created = DateTime.MinValue;
+    		DateTime modified = DateTime.MaxValue;
+    		
+    		var message = new MessageEventArgs_410420(messageid, messageName, path, fileType, fileSize, created, modified);
+    		List<MessageEventArgs_410420> list = new List<MessageEventArgs_410420>();
+    		list.Add(message);
+    		
+    		return list;
+    	}
+    
+    	[Test]
+    	public void Adding_the_same_object_twice_should_return_same_objects(){
+    		var childrens = getListWithOneChildren();
+    		
+    		parentFolder.AddChildren(childrens);
+    		INode children1 = parentFolder.Children[0];
+    		
+    		parentFolder.AddChildren(childrens);
+    		INode children2 = parentFolder.Children[0];
+    		
+    		Assert.That(parentFolder.Children.Count == 1, "Adding same object twice should only get one node");
+    		Assert.That(children1, Is.EqualTo(children2), "Node is not the same object");
+    	}
+    }
 }
