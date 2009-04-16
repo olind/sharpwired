621958153e554b1aa0c3e023abf5022c4c65ea21
lindberg.ola <lindberg.ola@cd302ba0-9ce7-11dd-979b-47109e308e52>
2009-04-16 18:57:05 +0000

    Started on folder transfers.

--- trunk/SharpWired/Model/Transfers/Transfers.cs
+++ trunk/SharpWired/Model/Transfers/Transfers.cs
@@ -3,41 +3,44 @@ using System.Collections.Generic;
 using SharpWired.Model.Files;
 
 namespace SharpWired.Model.Transfers {
-    public class Transfers : ModelBase {
-        private readonly List<ITransfer> transfers = new List<ITransfer>();
+	public class Transfers : ModelBase {
+		private readonly List<ITransfer> transfers = new List<ITransfer>();
 
-        public List<ITransfer> AllTransfers { get { return transfers; } }
+		public List<ITransfer> AllTransfers { get { return transfers; } }
 
-        public delegate void TransferDelegate(ITransfer t);
+		public delegate void TransferDelegate(ITransfer t);
 
-        public event TransferDelegate TransferAdded;
+		public event TransferDelegate TransferAdded;
 
-        public ITransfer Add(INode node, string target) {
-            return Add(node, target, 0);
-        }
+		public ITransfer Add(INode node, string target) {
+			return Add(node, target, 0);
+		}
 
-        public ITransfer Add(INode node, string target, Int64 offset) {
-            ITransfer transfer = null;
+		public ITransfer Add(INode node, string target, Int64 offset) {
+			ITransfer transfer = CreateTransfer(node, target, offset);
 
-            if (node is IFile) {
-                transfer = new FileTransfer((IFile)node, target, offset);
-            } else if (node is IFolder) {
-                transfer = new FolderTransfer((IFolder) node, target);
-            }
+			if (transfer != null) {
+				transfers.Add(transfer);
 
-            if (transfer != null) {
-                transfers.Add(transfer);
+				if (TransferAdded != null) {
+					TransferAdded(transfer);
+				}
+			}
+			return transfer;
+		}
 
-                if (TransferAdded != null) {
-                    TransferAdded(transfer);
-                }
-            }
-            return transfer;
-        }
+		public ITransfer CreateTransfer(INode node, string target, Int64 offset) {
+			if (node is IFile) {
+				return new FileTransfer((IFile)node, target, offset);
+			} else if (node is IFolder) {
+				return new FolderTransfer(this, (IFolder)node, target);
+			}
+			throw new ArgumentException("Transfer was not of type IFile or IFolder.");
+		}
 
-        public void Remove(ITransfer transfer) {
-            throw new NotImplementedException();
-            //Use the following event: public event TransferDelegate TransferRemoved;
-        }
-    }
+		public void Remove(ITransfer transfer) {
+			throw new NotImplementedException();
+			//Use the following event: public event TransferDelegate TransferRemoved;
+		}
+	}
 }
