21de6ae095e02ee7470cc1f109dbfd38d33209a4
lindberg.ola <lindberg.ola@cd302ba0-9ce7-11dd-979b-47109e308e52>
2009-05-26 17:47:16 +0000

    Added moq library and tried to set up one test. It fails with InvalidCastException and I don't know why. Will look into it later.

--- trunk/SharpWired/Model/Transfers/FolderTransfer.cs
+++ trunk/SharpWired/Model/Transfers/FolderTransfer.cs
@@ -65,6 +65,9 @@ namespace SharpWired.Model.Transfers {
         
         public FolderTransfer(Transfers transfers, IFolder node, string destination) {
         	Transfers = transfers;
+        	
+        	
+        	
             Source = (INode)node;
             Destination = destination;
             Status = Status.Idle;
---
+++ trunk/Tests/Model/Transfers/FolderTransferTest.cs
@@ -0,0 +1,23 @@
+using SharpWired.Model.Files;
+using System;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using SharpWired.Model.Transfers;
+
+namespace SharpWired.Tests.Model.Transfers {
+	[TestFixture]
+	public class FolderTransferTest {
+		
+		[Test, Ignore]
+		public void ShouldGetFolderSize() {
+			var node = new Mock<IFolder>();
+			node.Setup(x => x.Count).Returns(1);
+			
+			FolderTransfer transfer =  new FolderTransfer(null, node.Object, "");
+			var size = transfer.Size;
+			
+			Assert.That(size, Is.EqualTo(1));
+		}
+	}
+}
