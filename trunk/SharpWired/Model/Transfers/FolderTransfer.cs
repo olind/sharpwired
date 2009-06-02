28aa6bd3ab35cfe9a341ec99a8dd4f787708b9b8
lindberg.ola <lindberg.ola@cd302ba0-9ce7-11dd-979b-47109e308e52>
2009-06-02 20:04:13 +0000

    * Added interface for Commands (only used by testing at the moment). * Added tests for FolderTransfer.
    / Ola & Adam

--- trunk/SharpWired/Model/Transfers/FolderTransfer.cs
+++ trunk/SharpWired/Model/Transfers/FolderTransfer.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using SharpWired.Connection;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
-
 using SharpWired.Model.Files;
 
 namespace SharpWired.Model.Transfers {
@@ -10,97 +10,113 @@ namespace SharpWired.Model.Transfers {
         public string Destination { get; private set; }
         public INode Source { get; private set; }
         
+        private Status status;
         public Status Status { 
         	get {
-        		if(SubTransfers.Count == 0){
-        			//TODO: Should return done when we are sure no subfolders should exist for this?
-        		} else {
+        		if (((IFolder)Source).Count != 0) {
         			//TODO: Should return aggregated status of all subtransfers
-        		}
-        		return Status.Pending;
+        		} 
+        		return status;
         	} 
-        	set {
-        		
+        	private set {
+        		status = value;
         	}
         }
 
         public TimeSpan? EstimatedTimeLeft { get { throw new NotImplementedException(); } }
-
-        public double Progress { get; private set; }
-
+        public double Progress {
+        	get {
+        		if (((IFolder)Source).Count != 0) {
+        			// TODO: Should return mean value of all sub transfer statuses.
+        		} else if (Status == Status.Done) {
+        			return 1.0;
+        		}
+        		
+        		return 0.0;
+        	}
+        }
         public long Size { 
         	get {
-        		if(((IFolder)Source).Count == 0){
+        		if (((IFolder)Source).Count == 0){
         			return 0; 	
         		} else {
         			throw new NotImplementedException();
         		}
         	} 
         }
-
         public long Received { get { throw new NotImplementedException(); } }
-
         public long Speed { get { throw new NotImplementedException(); } }
-
-        private Transfers Transfers { get; set; }
-        private List<ITransfer> SubTransfers = new List<ITransfer>();
-
+        
         public event TransferDoneDelegate TransferDone;
 
+        private Transfers Transfers { get; set; }
+        private List<ITransfer> SubTransfers;
+        private bool listingDone;
+        private bool startCalled;
+
         private void OnNodeUpdated(INode node) {
-        	if(!node.Equals(Source)){
+        	if (!node.Equals(Source)){
         		throw new ArgumentException("Received updated callback for wrong node");
         	}
-
-        	Status = Status.Active;
-            Source.Updated -= OnNodeUpdated;
+        	
+        	Source.Updated -= OnNodeUpdated;
             
-            foreach(var child in ((IFolder)node).Children) {
+            foreach (var child in ((IFolder)node).Children) {
             	ITransfer t = Transfers.CreateTransfer(child, Path.Combine(Destination, child.Name), 0);
             	SubTransfers.Add(t);
             }
-            
-            Download();
+        	
+        	if (startCalled) {
+        		Download();
+        	} else {
+        		Status = Status.Pending;
+	        	listingDone = true;
+        	}
         }
         
-        public FolderTransfer(Transfers transfers, IFolder node, string destination) {
+        public FolderTransfer(ICommands commands, Transfers transfers, IFolder node, string destination) {
         	Transfers = transfers;
         	
             Source = (INode)node;
             Destination = destination;
             Status = Status.Idle;
-            Progress = 0.0;
+            SubTransfers = new List<ITransfer>();
+            
+        	Source.Updated += OnNodeUpdated;
+        	listingDone = false;
+            commands.List(Source.FullPath);
         }
 
         public void Start() {
-        	Status = Status.Pending;
-        	Source.Updated += OnNodeUpdated;
-            ConnectionManager.Commands.List(Source.FullPath);
+        	if (listingDone) {
+        		Download();
+        	} else {
+        		Status = Status.Pending;
+        		startCalled = true;
+        	}
         }
         
         private void Download() {
+        	Status = Status.Active;
             System.IO.Directory.CreateDirectory(Destination);
+            Status = Status.Done;
             
             foreach(var t in SubTransfers){
             	t.Start();
             }
-            
-            
-            Progress = 1.0;
-           	Status = Status.Done;
 
             if (TransferDone != null) {
                 TransferDone();
             }
-
-            //TODO: Create all files and folder transfers
         }
 
         public void Pause() {
+        	// TODO: Implement!
             throw new NotImplementedException();
         }
 
         public void Cancel() {
+        	// TODO: Implement!
             throw new NotImplementedException();
         }
     }
